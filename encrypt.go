da0643875515e2221ac7a0307455a2fda1c8d0a7
Will Young <1422781+lostnet@users.noreply.github.com>
2026-04-01 18:32:51 +0200

    EncryptionAlgorithmAES256CBC
    EncryptionAlgorithmAES256CBC

--- encrypt.go
+++ encrypt.go
@@ -61,7 +61,7 @@ const (
 // ContentEncryptionAlgorithm determines the algorithm used to encrypt the
 // plaintext message. Change the value of this variable to change which
 // algorithm is used in the Encrypt() function.
-var ContentEncryptionAlgorithm = EncryptionAlgorithmAES128CBC
+var ContentEncryptionAlgorithm = EncryptionAlgorithmAES256CBC
 
 // ErrUnsupportedEncryptionAlgorithm is returned when attempting to encrypt
 // content with an unsupported algorithm.
